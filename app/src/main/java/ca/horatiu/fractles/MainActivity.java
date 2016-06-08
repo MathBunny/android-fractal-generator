7dc053ec3fde7d5c1999e9a732f07dedef30e7ff
Horatiu Lazu <horatiulazu@gmail.com>
2016-06-07 23:16:14 -0400

    Add bitmap optimization

--- app/src/main/java/ca/horatiu/fractles/MainActivity.java
+++ app/src/main/java/ca/horatiu/fractles/MainActivity.java
@@ -11,6 +11,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+
 
 
 
@@ -22,16 +25,6 @@ public class MainActivity extends Activity {
         setContentView(renderScreen);
 
         Intent intent = getIntent();
-        /*
-        <string name="color_shift">Color Shift:</string>
-        <string name="quality_depth">Quality (Depth):</string>
-        <string name="_default_scale">8</string>
-        <string name="_default_zoom">500</string>
-        <string name="default_max">Default (Max)</string>
-        <string name="default_max_width">Default (Max)</string>
-        <string name="_default_color">10</string>
-        <string name="_default_quality">570</string>
-        */
 
         renderScreen.CONSTANT_SHIFT = intent.getIntExtra(CustomizationScreen.COLOR_SHIFT, 10);
         renderScreen.MAX_ITER = intent.getIntExtra(CustomizationScreen.QUALITY_DEPTH, 570);
@@ -43,15 +36,12 @@ public class MainActivity extends Activity {
         //CustomizationScreen.
     }
 
-
-
     public class MyView extends View {
         double ZOOM = 500;
         int MAX_ITER =  570;
         int CONSTANT_SHIFT = 10;
         int ADDITIVE_FACTOR = 3;
         boolean once = false;
-
 
         Paint paint;
 
@@ -60,10 +50,59 @@ public class MainActivity extends Activity {
             paint = new Paint();
         }
 
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
+        public void mandelbrotRenderBMP(Canvas canvas){
+            Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
+            Bitmap bmp = Bitmap.createBitmap(getWidth(), getHeight(), conf);
 
+            double zx, zy, cX, cY, tmp;
+
+            Log.d("Fractals", "hi!" + ADDITIVE_FACTOR);
+
+            for (int y = 0; y < getHeight(); y+= ADDITIVE_FACTOR) {
+                for (int x = 0; x < getWidth(); x+= ADDITIVE_FACTOR) {
+                    zx = zy = 0;
+                    cX = (x - 400) / ZOOM;
+                    cY = (y - 300) / ZOOM;
+                    int iter = MAX_ITER;
+                    while (zx * zx + zy * zy < 4 && iter > 0) {
+                        tmp = zx * zx - zy * zy + cX;
+                        zy = 2.0 * zx * zy + cY;
+                        zx = tmp;
+                        iter--;
+                    }
+                    Paint color = new Paint();
+                    int binaryColorRepresentation = (iter | iter << CONSTANT_SHIFT);
+                    int blue = binaryColorRepresentation & 0xff;
+                    int green = (binaryColorRepresentation & 0xff00) >> 8;
+                    int red = (binaryColorRepresentation & 0xff0000) >> 16;
+                    paint.setColor(Color.parseColor(String.format("#%02x%02x%02x", red, green, blue)));  //using paint instead :)
+                    for(int xx = 0; xx < ADDITIVE_FACTOR; xx++){
+                        for(int yy = 0; yy < ADDITIVE_FACTOR; yy++){
+                            if (xx + x >= getWidth() || yy+y >= getHeight())
+                                continue;
+                            bmp.setPixel(xx+x, yy+y, Color.rgb(red, green,  blue));
+                        }
+                    }
+
+
+                    //canvas.drawRect(x, y, x + ADDITIVE_FACTOR, y + ADDITIVE_FACTOR, paint);
+                }
+            }
+            //Canvas bmpCanvas = new Canvas(bmp);
+            //Insert all the rest of the drawing commands here
+
+            //Canvas myCanvas = new Canvas(bmp);
+//Insert all the rest of the drawing commands here
+            //canvas.drawBitmap(myCanvas, 0, 0);
+
+
+            //canvas.setBitmap(bmp);
+
+            canvas.drawBitmap(bmp, new Matrix(), null);
+            Log.d("Fractals", "Done!");
+        }
+
+        public void mandelbrotRender(Canvas canvas){
             int width = getWidth();
             int height = getHeight();
             int radius;
@@ -111,6 +150,13 @@ public class MainActivity extends Activity {
 
                 canvas.drawCircle(width / 2, height / 2, 10, paint);*/
             }
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            //mandelbrotRender(canvas);
+            mandelbrotRenderBMP(canvas);
         }
     }
 }
